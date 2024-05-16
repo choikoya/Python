469855832ffa32aa698d2ab3294a2deaeb4aecb4
choikoya <choikoyaaa@gmail.com>
2024-05-16 18:59:32 +0900

    0516

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+message = "hello\'s python"
+print(message)
+name = "ada lovelace"
+# 첫글자 대문자
+print(name.title()) 
+number = 10
+print(number.as_integer_ratio())
+first_name="gildong"
+last_name="hong   "
+# title 쓴거 안쓴거 비교
+# strip 공백없애줌
+full_name=f"{last_name.title().strip()} {first_name}"
+print(full_name)`   `
